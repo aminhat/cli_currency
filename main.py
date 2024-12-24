fce88ec84fc549fe7a0dfce9ee5c9c746cf24431
Aminmdge <m.amin.h.2646@gmail.com>
2024-12-24 18:20:05 +0330

    report function developed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     import argparse
-    from cli import add, list_all, list_filter
+    from cli import add, list_all, list_filter, report
 
     parser = argparse.ArgumentParser(description='My Python CLI Application')
     subparsers = parser.add_subparsers(dest='command')
@@ -10,11 +10,11 @@ def main():
     add_parser.add_argument('value', type=int, help='Value of currency')
 
     list_parser = subparsers.add_parser('list', help='List all currencies')
-
     list_parser.add_argument('-filter', action='store_true', help='Filter by name')
     list_parser.add_argument('filter_name', type=str, nargs='?', help='Name of currency to be filtered')
-
     list_parser.add_argument('-sort', action='store_true', help='Sort output by value')
+
+    report_parser = subparsers.add_parser('report', help='Report min and max value of each currency')
 
     args = parser.parse_args()
 
@@ -25,7 +25,8 @@ def main():
             list_filter(args.filter_name, args.sort)
         else:
             list_all(args.sort)
-
+    elif args.comand == 'report':
+        report()
 
 if __name__ == '__main__':
     main()

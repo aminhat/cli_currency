3f492dca3a5ac91d8032f1eea137245a5f59abfe
Aminmdge <m.amin.h.2646@gmail.com>
2025-01-01 01:54:05 +0330

    WHATCHME.cast file added and terminal recorded in it working with cli, also the README.md and help of commands updated

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cli_currency
+a cli about currencies and monitoring their information.
+see --help
+you can use WHATCHME.cast with asciinema command.
+install it then run this command : asciinema play WHATCHME.cast
--- main.py
+++ main.py
@@ -2,23 +2,23 @@ def main():
     import argparse
     from cli.commands import add, listc, report
 
-    parser = argparse.ArgumentParser(description='My Python CLI Application')
+    parser = argparse.ArgumentParser(description='My Python CLI Application about currencies')
     subparsers = parser.add_subparsers(dest='command')
 
     add_parser = subparsers.add_parser('add', help='Add a currency')
     add_parser.add_argument('name', type=str, help='Name of currency')
     add_parser.add_argument('value', type=float, help='Value of currency')
 
-    list_parser = subparsers.add_parser('list', help='List all currencies')
+    list_parser = subparsers.add_parser('list', help="List all currencies")
     list_parser.add_argument('-f', '--filter', action='store_true', help='Filter by name')
     list_parser.add_argument('filter_name', type=str, nargs='?', help='Name of currency to be filtered')
-    list_parser.add_argument('-s', '--sort', action='store_true', help='Sort output by value')
+    list_parser.add_argument('-s', '--sort', action='store_true', help='Sort output by value or date')
     list_parser.add_argument('--asc', action='store_true', help='Sort in ascending order')
-    list_parser.add_argument('--desc', action='store_true', help='Sort in descending order')
-    list_parser.add_argument('--price', action='store_true', help='Sort by value')
+    list_parser.add_argument('--desc', action='store_true', help='Sort in descending order (default)')
+    list_parser.add_argument('--price', action='store_true', help='Sort by value (default)')
     list_parser.add_argument('--date', action='store_true', help='Sort by date created at')
 
-    report_parser = subparsers.add_parser('report', help='Report min and max value of each currency')
+    report_parser = subparsers.add_parser('report', help='Report minimum and maximum value of each currency')
 
     args = parser.parse_args()
 
